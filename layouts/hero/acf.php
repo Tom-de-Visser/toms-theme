3cccd8dda122f7eff3cf769757dfcbb81eb2ab23
Tom de Visser <tomdevisser29@gmail.com>
2024-12-20 21:42:20 +0100

    Added button component, and some more hero fields

--- layouts/hero/acf.php
+++ layouts/hero/acf.php
@@ -14,10 +14,29 @@ $layouts[] = array(
 	'display'    => 'block',
 	'sub_fields' => array(
 		array(
-			'key'   => 'field_' . $layout_name . '_title',
-			'label' => __( 'Title', 'toms' ),
-			'name'  => $layout_name . '_title',
+			'key'   => 'field_' . $layout_name . '_pretitle',
+			'label' => __( 'Pretitle', 'toms' ),
+			'name'  => $layout_name . '_pretitle',
 			'type'  => 'text',
+		),
+		array(
+			'key'      => 'field_' . $layout_name . '_title',
+			'label'    => __( 'Title', 'toms' ),
+			'name'     => $layout_name . '_title',
+			'type'     => 'text',
+			'required' => true,
+		),
+		array(
+			'key'   => 'field_' . $layout_name . '_text',
+			'label' => __( 'Text', 'toms' ),
+			'name'  => $layout_name . '_text',
+			'type'  => 'text',
+		),
+		array(
+			'key'   => 'field_' . $layout_name . '_button',
+			'label' => __( 'Button', 'toms' ),
+			'name'  => $layout_name . '_button',
+			'type'  => 'link',
 		),
 	),
 );
--- layouts/hero/hero.php
+++ layouts/hero/hero.php
@@ -6,8 +6,40 @@
  */
 
 $layout_name = 'hero';
-$hero_title  = get_sub_field( $layout_name . '_title' );
+
+$hero_title    = get_sub_field( $layout_name . '_title' );
+$hero_pretitle = get_sub_field( $layout_name . '_pretitle' );
+$hero_text     = get_sub_field( $layout_name . '_text' );
+$hero_button   = get_sub_field( $layout_name . '_button' );
 ?>
 <section class="<?php echo esc_attr( $layout_name ); ?> pt-24 md:mx-12 lg:mx-24">
-	<h1><?php echo wp_kses_post( $hero_title ); ?></h1>
+	<div class="flex flex-col gap-2">
+		<div class="flex flex-col">
+			<?php
+			if ( $hero_pretitle ) {
+				?>
+				<span class="text-sm text-gray-500"><?php echo wp_kses_post( $hero_pretitle ); ?></span>
+				<?php
+			}
+			?>
+
+			<h1 class="text-2xl"><?php echo wp_kses_post( $hero_title ); ?></h1>
+
+			<?php
+			if ( $hero_text ) {
+				?>
+				<p><?php echo wp_kses_post( $hero_text ); ?></p>
+				<?php
+			}
+			?>
+		</div>
+
+		<?php
+		if ( $hero_button ) {
+			?>
+			<a href="<?php echo esc_url( $hero_button['url'] ); ?>" class="button"><?php echo esc_html( $hero_button['title'] ); ?></a>
+			<?php
+		}
+		?>
+	</div>
 </section>
