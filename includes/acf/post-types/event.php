a18fa2f89f43e0a4ab85fae90249f6e8177546b1
Tom de Visser <tomdevisser29@gmail.com>
2024-12-21 15:08:22 +0100

    Refactored the way fields are registered for post types, and added some fields for the event post type

---
+++ includes/acf/post-types/event.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Register ACF fields for events.
+ *
+ * @package toms
+ */
+
+/**
+ * Register ACF fields for events.
+ *
+ * @return void
+ */
+function toms_add_event_fields(): void {
+	acf_add_local_field_group(
+		array(
+			'key'                   => 'group_event_fields',
+			'title'                 => __( 'Event', 'toms' ),
+			'fields'                => array(
+				array(
+					'key'       => 'field_event_date',
+					'label'     => __( 'Date', 'toms' ),
+					'name'      => 'date',
+					'type'      => 'date_time_picker',
+					'first_day' => 1,
+					'wrapper'   => array(
+						'width' => '50',
+					),
+				),
+				array(
+					'key'     => 'field_event_price',
+					'label'   => __( 'Price', 'toms' ),
+					'name'    => 'price',
+					'type'    => 'text',
+					'wrapper' => array(
+						'width' => '50',
+					),
+				),
+				array(
+					'key'     => 'field_event_location',
+					'label'   => __( 'Location', 'toms' ),
+					'name'    => 'location',
+					'type'    => 'text',
+					'wrapper' => array(
+						'width' => '50',
+					),
+				),
+				array(
+					'key'     => 'field_event_booking_url',
+					'label'   => __( 'Booking URL', 'toms' ),
+					'name'    => 'booking_url',
+					'type'    => 'url',
+					'wrapper' => array(
+						'width' => '50',
+					),
+				),
+				array(
+					'key'   => 'field_event_description',
+					'label' => __( 'Description', 'toms' ),
+					'name'  => 'description',
+					'type'  => 'wysiwyg',
+				),
+			),
+			'location'              => array(
+				array(
+					array(
+						'param'    => 'post_type',
+						'operator' => '==',
+						'value'    => 'event',
+					),
+				),
+			),
+			'position'              => 'acf_after_title',
+			'label_placement'       => 'top',
+			'instruction_placement' => 'label',
+			'active'                => true,
+			'hide_on_screen'        => array(
+				0 => 'the_content',
+			),
+		),
+	);
+}
+add_action( 'acf/include_fields', 'toms_add_event_fields' );
---
+++ includes/acf/theme-settings.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Register ACF theme settings.
+ *
+ * @package toms
+ */
+
+/**
+ * Register ACF theme settings.
+ *
+ * @return void
+ */
+function toms_add_theme_settings(): void {
+	acf_add_options_page(
+		array(
+			'page_title' => __( 'Theme Settings', 'strl' ),
+			'menu_title' => __( 'Theme Settings', 'strl' ),
+			'menu_slug'  => 'theme-settings',
+			'capability' => 'edit_posts',
+			'redirect'   => false,
+		),
+	);
+
+	acf_add_local_field_group(
+		array(
+			'key'                   => 'field_theme_settings',
+			'title'                 => __( 'Theme Settings', 'strl' ),
+			'fields'                => array(
+				array(
+					'key'       => 'field_theme_settings_general_tab',
+					'label'     => __( 'General', 'strl' ),
+					'name'      => 'general_tab',
+					'type'      => 'tab',
+					'placement' => 'left',
+				),
+				array(
+					'key'        => 'field_theme_settings_branding',
+					'label'      => __( 'Branding', 'strl' ),
+					'name'       => 'branding',
+					'type'       => 'group',
+					'sub_fields' => array(
+						array(
+							'key'   => 'field_theme_settings_branding_logo',
+							'label' => __( 'Logo', 'strl' ),
+							'name'  => 'logo',
+							'type'  => 'image',
+						),
+					),
+				),
+				array(
+					'key'       => 'field_theme_settings_socials_tab',
+					'label'     => __( 'Socials', 'strl' ),
+					'name'      => 'socials_tab',
+					'type'      => 'tab',
+					'placement' => 'left',
+				),
+				array(
+					'key'        => 'field_theme_settings_socials',
+					'label'      => __( 'Socials', 'strl' ),
+					'name'       => 'socials',
+					'type'       => 'group',
+					'sub_fields' => array(
+						array(
+							'key'   => 'field_theme_settings_socials_github',
+							'label' => __( 'GitHub', 'strl' ),
+							'name'  => 'github',
+							'type'  => 'url',
+						),
+						array(
+							'key'   => 'field_theme_settings_socials_linkedin',
+							'label' => __( 'LinkedIn', 'strl' ),
+							'name'  => 'linkedin',
+							'type'  => 'url',
+						),
+						array(
+							'key'   => 'field_theme_settings_socials_instagram',
+							'label' => __( 'Instagram', 'strl' ),
+							'name'  => 'instagram',
+							'type'  => 'url',
+						),
+					),
+				),
+			),
+			'location'              => array(
+				array(
+					array(
+						'param'    => 'options_page',
+						'operator' => '==',
+						'value'    => 'theme-settings',
+					),
+				),
+			),
+			'menu_order'            => 0,
+			'label_placement'       => 'top',
+			'instruction_placement' => 'label',
+			'active'                => true,
+		)
+	);
+}
+add_action( 'acf/include_fields', 'toms_add_theme_settings' );
